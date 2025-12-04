13fbb6ebbdbaf548efc84c82b1d6af3346458519
TheCodedHuman <mokshmahawar05@gmail.com>
2025-12-04 21:17:12 +0530

    Create main application structure for ChronoWish

---
+++ main.py
@@ -0,0 +1,35 @@
+# Here we are fabricating the starting point of the ChronoWish app
+
+# Imports
+from tkinter import Tk
+from tkinter.ttk import Frame
+
+
+# Defined
+
+
+
+# Classed
+class Base:
+    def __init__(self, root):
+
+        # TopLevel Frame
+        self.topLevel = Frame(root, padding=15)
+        self.topLevel.grid(row=0, column=0, sticky="news")
+        self.topLevel.columnconfigure(0, weight=1)
+
+
+
+# Literals
+
+
+
+# Main
+def main():
+    root = Tk()
+    root.columnconfigure(0, weight=1)
+    root.rowconfigure(0, weight=1)
+    Base(root)
+    root.mainloop()
+main()
+
